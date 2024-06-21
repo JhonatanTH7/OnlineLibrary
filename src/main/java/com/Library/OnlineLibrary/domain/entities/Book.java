fef4b0d1fd60ca9d0ca99b7c78d2bbe41444f692
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-06-21 07:20:18 -0500

    AllArgsConstructor added to entities

--- src/main/java/com/Library/OnlineLibrary/domain/entities/Book.java
+++ src/main/java/com/Library/OnlineLibrary/domain/entities/Book.java
@@ -9,10 +9,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "book")
 public class Book {
--- src/main/java/com/Library/OnlineLibrary/domain/entities/Loan.java
+++ src/main/java/com/Library/OnlineLibrary/domain/entities/Loan.java
@@ -10,10 +10,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "loan")
 public class Loan {
